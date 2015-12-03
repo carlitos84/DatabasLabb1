06d9d53a79e4050e9f5b3ebc4830e3de58a5c0b3
Highflex69 <roland_5_26@hotmail.com>
2015-12-03 21:32:27 +0100

    right files

---
+++ Model/User.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Teddy
+ */
+public class User {
+    private int ID;
+    private String name;
+    
+    public User(int id, String name)
+    {
+        this.ID = id;
+        this.name = name;
+    }
+    
+    public int getID()
+    {
+        return this.ID;
+    }
+    
+    public String getName()
+    {
+        return this.name;
+    }   
+}
