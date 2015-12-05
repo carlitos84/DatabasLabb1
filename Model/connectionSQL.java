aaab41336c028d2d7c8bd64d1948ffe78396a869
carlitos84 <cgaldo@kth.se>
2015-12-05 16:05:42 +0100

    connectionSQL lägg till album fungerar
    Om artist inte existerar när man lägger till album så bör en exception
    skapas så att gui vet om det. Det finns en if sats i metoden addAlbum
    som kollar om artisten existerar. Guin bör isåfall promta användaren så
    att artisten skapas först innan albumet kan läggas till om det blir
    exeption

--- Model/connectionSQL.java
+++ Model/connectionSQL.java
@@ -34,6 +34,15 @@ public class connectionSQL {
         
     }
     
+    public connectionSQL(String user, String pwd) {
+        database = "labb1";
+        server = "jdbc:mysql://localhost:3306/" + database;
+        this.user = user;
+        this.pwd = pwd;
+        
+        con = null;
+    }
+    
     public ArrayList<Artist> searchForArtistByName(String searchString) {
         ArrayList<Artist> artistList = new ArrayList<>();
         try {
@@ -80,5 +89,69 @@ public class connectionSQL {
                 con.close();
             }catch(SQLException e){}
         }
+    }
+    
+    public void addAlbum(String title, String genre, String artistName) {
+        
+        if(!checkIfArtisExists(artistName) ) {
+            //Exception bör skapas
+        }
+        else {
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                
+                con = DriverManager.getConnection(server, user, pwd);
+                String sqlGetId = "select K_Id from T_Artist where K_Name = ?";
+                PreparedStatement getArtistId = con.prepareStatement(sqlGetId);
+                
+                getArtistId.setString(1, artistName);
+                ResultSet rs = getArtistId.executeQuery();
+                boolean next = rs.next();
+                int artistId = rs.getInt(1);
+                
+                String sql = "insert into T_Album(K_Title, K_Genre, K_MadeBy) "
+                            +"values(?, ?, ?)";
+                PreparedStatement insertAlbum = con.prepareStatement(sql);
+                insertAlbum.setString(1, title);
+                insertAlbum.setString(2, genre);
+                insertAlbum.setInt(3, artistId);
+                int n = insertAlbum.executeUpdate();
+            
+            }catch(Exception e) {}
+            finally {
+                try {
+                    con.close();
+                }catch(SQLException e) {}
+            }
+        
+        }
+    }
+    
+    private boolean checkIfArtisExists(String artistName) {
+        
+        boolean artistFound = false;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(server, user, pwd);    
+            
+            String sql = "select count(K_Id) from T_Artist where K_Name = ?";
+            PreparedStatement checkArtistExistance = con.prepareStatement(sql);
+            checkArtistExistance.setString(1, artistName);      
+            
+            ResultSet rs = checkArtistExistance.executeQuery();
+            
+            boolean next = rs.next();
+            if(rs.getInt(1) > 0) {
+                artistFound = true;
+            }
+            
+        }catch(Exception e) {}
+        finally {
+            try {
+                con.close();
+            }catch(SQLException e) {}
+        }
+        
+        return artistFound;
     }
 }
